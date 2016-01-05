aedd50fb85e1ccddcddcc24825ed1b4c73528640
Troels Henriksen <athas@sigkill.dk>
2016-01-05 17:57:45 +0100

    Bedre Huffman

--- bankopak/algorithm_5.h
+++ bankopak/algorithm_5.h
@@ -9,93 +9,98 @@
 #include "bankopladeformat/bankopladeformat.h"
 #include "bitio.h"
 
+unsigned int a5_huffman_decode(FILE *in, size_t maplen, const char** map_in) {
+  char **map = malloc(sizeof(const char*) * maplen);
+  memcpy(map, map_in, sizeof(const char*) * maplen);
 
-unsigned int a5_read_next(FILE *in) {
-  if (read_bit(in) == 0) {
-    return 0;
-  } else {
-    if (read_bit(in) == 0) {
-      if (read_bit(in) == 0) {
-        if (read_bit(in) == 0) {
-          return 1;
-        } else {
-          return 2;
-        }
-      } else {
-        if (read_bit(in) == 0) {
-          return 3;
-        } else {
-          return 4;
-        }
+  while (1) {
+    int b = read_bit(in);
+
+    if (b == EOF) {
+      free(map);
+      return EOF;
+    }
+
+    int hit, hits = 0;
+    for (size_t i = 0; i < maplen; i++) {
+      if (map[i] == NULL) {
+        continue;
       }
-    } else {
-      if (read_bit(in) == 0) {
-        if (read_bit(in) == 0) {
-          return 5;
-        } else {
-          return 6;
-        }
+
+      if (b + '0' == map[i][0]) {
+        map[i]++;
+        hit = i;
+        hits++;
       } else {
-        if (read_bit(in) == 0) {
-          return 7;
-        } else {
-          if (read_bit(in) == 0) {
-            return 8;
-          } else {
-            if (read_bit(in) == 0) {
-              return 9;
-            } else {
-              if (read_bit(in) == 0) {
-                return 10;
-              } else {
-                return 11;
-              }
-            }
-          }
-        }
+        map[i] = NULL;
       }
+    }
+    if (hits == 1) {
+      free(map);
+      return hit;
     }
   }
 }
 
-void a5_write_next(unsigned int c, FILE *out) {
-  switch (c) {
-  case 0:
-    write_bit(0, out);
-    return;
-  case 1:
-    write_bit(1, out); write_bit(0, out); write_bit(0, out); write_bit(0, out);
-    return;
-  case 2:
-    write_bit(1, out); write_bit(0, out); write_bit(0, out); write_bit(1, out);
-    return;
-  case 3:
-    write_bit(1, out); write_bit(0, out); write_bit(1, out); write_bit(0, out);
-    return;
-  case 4:
-    write_bit(1, out); write_bit(0, out); write_bit(1, out); write_bit(1, out);
-    return;
-  case 5:
-    write_bit(1, out); write_bit(1, out); write_bit(0, out); write_bit(0, out);
-    return;
-  case 6:
-    write_bit(1, out); write_bit(1, out); write_bit(0, out); write_bit(1, out);
-    return;
-  case 7:
-    write_bit(1, out); write_bit(1, out); write_bit(1, out); write_bit(0, out);
-    return;
-  case 8:
-    write_bit(1, out); write_bit(1, out); write_bit(1, out); write_bit(1, out); write_bit(0, out);
-    return;
-  case 9:
-    write_bit(1, out); write_bit(1, out); write_bit(1, out); write_bit(1, out); write_bit(1, out); write_bit(0, out);
-    return;
-  case 10:
-    write_bit(1, out); write_bit(1, out); write_bit(1, out); write_bit(1, out); write_bit(1, out); write_bit(1, out); write_bit(0, out);
-    return;
-  case 11:
-    write_bit(1, out); write_bit(1, out); write_bit(1, out); write_bit(1, out); write_bit(1, out); write_bit(1, out); write_bit(1, out);
-    return;
+void a5_huffman_encode(unsigned int c, FILE *out, const char** map) {
+  const char *s = map[c];
+
+  while (*s) {
+    write_bit(*s - '0', out);
+    s++;
+  }
+}
+
+const char* huffman_table_default[] =
+  {
+    "0",
+    "1000",
+    "1001",
+    "1010",
+    "1011",
+    "1100",
+    "1101",
+    "1110",
+    "11110",
+    "111110",
+    "1111110",
+    "1111111",
+  };
+
+const char* huffman_table_notfirst[] =
+  {
+    "0",
+    "1000",
+    "1001",
+    "1010",
+    "1011",
+    "1100",
+    "1101",
+    "1110",
+    "11110",
+    "111110",
+    "111111",
+  };
+
+unsigned int a5_read_next(int row, int col, int prev, int hasprev, FILE *in) {
+  row = row;
+  col = col;
+  prev = prev;
+  if (hasprev) {
+    return a5_huffman_decode(in, 12, huffman_table_notfirst);
+  } else {
+    return a5_huffman_decode(in, 12, huffman_table_default);
+  }
+}
+
+void a5_write_next(int row, int col, int prev, int hasprev, unsigned int c, FILE *out) {
+  row = row;
+  col = col;
+  prev = prev;
+  if (hasprev) {
+    return a5_huffman_encode(c, out, huffman_table_notfirst);
+  } else {
+    return a5_huffman_encode(c, out, huffman_table_default);
   }
 }
 
@@ -107,15 +112,17 @@ void a5_compress(FILE *out, FILE *in) {
   while (banko_reader_board(&reader, &board) == 0) {
     for (int col = 0; col < BOARD_COLS; col++) {
       uint8_t prev = col*10;
+      int hasprev = 0;
       for (int row = 0; row < BOARD_ROWS; row++) {
         uint8_t cell = board.cells[row][col];
         if (cell == 0) {
-          a5_write_next(0, out);
+          a5_write_next(row, col, prev, hasprev, 0, out);
         } else {
-          a5_write_next(cell - prev + 1, out);
+          a5_write_next(row, col, prev, hasprev, cell - prev + 1, out);
           prev = cell;
+          hasprev = 1;
         }
-      }
+       }
     }
   }
 
@@ -132,16 +139,18 @@ void a5_decompress(FILE *out, FILE *in) {
   while (1) {
     for (int col = 0; col < BOARD_COLS; col++) {
       uint8_t prev = col*10;
+      int hasprev = 0;
       for (int row = 0; row < BOARD_ROWS; row++) {
         if (peek_bit(in) == EOF) {
           goto end;
         }
 
-        uint8_t cell = a5_read_next(in);
+        uint8_t cell = a5_read_next(row, col, prev, hasprev, in);
         if (cell == 0) {
           board.cells[row][col] = cell;
         } else {
           prev = board.cells[row][col] = prev + cell - 1;
+          hasprev = 1;
         }
       }
     }
