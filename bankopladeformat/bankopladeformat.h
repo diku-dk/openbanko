be771a73eb58fcf0b760f2cf9804666055dc9cc9
Troels Henriksen <athas@sigkill.dk>
2018-07-16 23:00:37 +0200

    Add bankosort.

--- bankopladeformat/bankopladeformat.h
+++ bankopladeformat/bankopladeformat.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 /*
  * Library interface.
@@ -36,7 +37,13 @@ struct banko_reader {
 static int banko_reader_open(struct banko_reader *reader, FILE *file);
 static int banko_reader_board(struct banko_reader *reader, struct board *board);
 static int banko_reader_close(struct banko_reader *reader);
+
+/* Clear the error state (when is this useful?). */
 static void banko_reader_clear_error(struct banko_reader *reader);
+
+/* More advanced functions built with the above. */
+static void banko_write_all_boards(FILE *file, struct board* boards, int nboards);
+static int banko_read_all_boards(FILE *file, struct board** boards, int *nboards);
 
 /*
  * All function definitions inlined and defined statically.
@@ -125,5 +132,43 @@ static void banko_reader_clear_error(struct banko_reader *reader) {
   reader->error = 0;
 }
 
+static void banko_write_all_boards(FILE *file, struct board* boards, int nboards) {
+  struct banko_writer writer;
+
+  banko_writer_open(&writer, file);
+
+  for (int i = 0; i < nboards; i++) {
+    banko_writer_board(&writer, &boards[i]);
+  }
+
+  banko_writer_close(&writer);
+}
+
+static int banko_read_all_boards(FILE *in, struct board **boards_out, int *nboards) {
+  struct board board;
+  struct banko_reader reader;
+  banko_reader_open(&reader, in);
+  int capacity = 100, used = 0, error = 0;
+
+  struct board *boards = malloc(capacity * sizeof(struct board));
+
+  while (banko_reader_board(&reader, &board) == 0) {
+    boards[used++] = board;
+
+    if (used == capacity) {
+      capacity *= 2;
+      boards = realloc(boards, capacity * sizeof(struct board));
+    }
+  }
+
+  banko_reader_close(&reader);
+
+  /* Trim allocation. */
+  boards = realloc(boards, used * sizeof(struct board));
+  *boards_out = boards;
+  *nboards = used;
+
+  return error;
+}
 
 #endif
---
+++ bankopladeformat/bankosort.c
@@ -0,0 +1,41 @@
+/*
+ * sort(1), but for .bankopladeformat-files.  Sorts lexicographically.
+ * Reads on stdin and outputs on stdout.  Manifests all boards in
+ * memory, so unsuitable for gigantic data sets.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <error.h>
+#include <errno.h>
+#include <string.h>
+
+#include "bankopladeformat.h"
+
+static int cmp_boards(const void *xp, const void *yp) {
+  const struct board *x = xp;
+  const struct board *y = yp;
+
+  return memcmp(x->cells, y->cells, BOARD_ROWS * BOARD_COLS);
+}
+
+int main(int argc, char** argv) {
+  if (argc != 1) {
+    fprintf(stderr, "Usage: %s < input > utput\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  struct board *boards;
+  int nboards;
+
+  if (banko_read_all_boards(stdin, &boards, &nboards) != 0) {
+    fprintf(stderr, "%s: failed reading boards from stdin.\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  qsort(boards, nboards, sizeof(struct board), cmp_boards);
+
+  banko_write_all_boards(stdout, boards, nboards);
+
+  return 0;
+}
